a9418455b2e9d62d66662ee158f0997d125a1a99
Whithris <visualstudiouser7@gmail.com>
2023-12-11 20:07:37 +0300

    switching to taichi

---
+++ camera.py
@@ -0,0 +1,89 @@
+from matrix_functions import *
+from PySide6.QtGui import QKeyEvent
+from PySide6.QtCore import Qt
+from math import pi
+import taichi as ti
+
+
+class Camera(object):
+    def __init__(self, render, position):
+        self.render = render
+        self.position = np.array([*position, 1.0])
+        self.forward = np.array([0, 0, 1, 1])
+        self.up = np.array([0, 1, 0, 1])
+        self.right = np.array([1, 0, 0, 1])
+        self.h_fov = pi / 3
+        self.v_fov = self.h_fov * (render.HEIGHT / render.WIDTH)
+        self.near_plane = 0.1
+        self.far_plane = 100
+        self.moving_speed = 0.3
+        self.rotation_speed = 0.015
+
+        self.anglePitch = 0
+        self.angleYaw = 0
+        self.angleRoll = 0
+
+    def control(self, e):
+        if e == 'a':
+            self.position -= self.right * self.moving_speed
+        if e == 'd':
+            self.position += self.right * self.moving_speed
+        if e == 'w':
+            self.position += self.forward * self.moving_speed
+        if e == 's':
+            self.position -= self.forward * self.moving_speed
+        if e == 'q':
+            self.position += self.up * self.moving_speed
+        if e == 'e':
+            self.position -= self.up * self.moving_speed
+        if e == ti.GUI.LEFT:
+            self.camera_yaw(-self.rotation_speed)
+        if e == ti.GUI.RIGHT:
+            self.camera_yaw(self.rotation_speed)
+        if e == ti.GUI.UP:
+            self.camera_pitch(self.rotation_speed)
+        if e == ti.GUI.DOWN:
+            self.camera_pitch(-self.rotation_speed)
+
+    def camera_yaw(self, angle):
+        self.angleYaw += angle
+
+    def camera_pitch(self, angle):
+        self.anglePitch += angle
+
+    def axiiIdentity(self):
+        self.forward = np.array([0, 0, 1, 1])
+        self.up = np.array([0, 1, 0, 1])
+        self.right = np.array([1, 0, 0, 1])
+
+    def camera_update_axii(self):
+        # rotate = rotate_y(self.angleYaw) @ rotate_x(self.anglePitch)
+        rotate = rotate_x(self.anglePitch) @ rotate_y(self.angleYaw)  # this concatenation gives right visual
+        self.axiiIdentity()
+        self.forward = self.forward @ rotate
+        self.right = self.right @ rotate
+        self.up = self.up @ rotate
+
+    def camera_matrix(self):
+        self.camera_update_axii()
+        return self.translate_matrix() @ self.rotate_matrix()
+
+    def translate_matrix(self):
+        x, y, z, w = self.position
+        return np.array([
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [-x, -y, -z, 1]
+        ])
+
+    def rotate_matrix(self):
+        rx, ry, rz, w = self.right
+        fx, fy, fz, w = self.forward
+        ux, uy, uz, w = self.up
+        return np.array([
+            [rx, ux, fx, 0],
+            [ry, uy, fy, 0],
+            [rz, uz, fz, 0],
+            [0, 0, 0, 1]
+        ])
